7d1dd3536618a40c0ba758e7f30a5cafc46c16d1
joshdels <deleonjoshy2@gmail.com>
2025-09-13 21:43:19 +0800

    first upload of react files

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>The New Blog</h1>
+            <div className="links"></div>
+              <a href="/">Home</a>
+              <a href="/create">New Blog</a>
+        </nav>
+     );
+}
+
+
+
+export default Navbar;
